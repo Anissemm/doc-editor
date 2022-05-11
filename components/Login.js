41b5c331fdc297945b34568c70173d05e6261ba7
Anissem <dima.anissem@gmail.com>
2022-05-11 11:42:49 +0100

    Add login page

--- components/Login.js
+++ components/Login.js
@@ -1,10 +1,10 @@
 import { motion } from "framer-motion"
 import Button from "@material-tailwind/react/Button"
 import Icon from "@material-tailwind/react/Icon"
-import { ReactComponent as Google } from '../assets/google.svg'
+import GoogleSvg from '../assets/svg/google'
 
 const MotionButton = motion(Button)
-
+console.log(GoogleSvg)
 const variants = {
     hidden: {
         opacity: 0,
@@ -13,8 +13,8 @@ const variants = {
     visible: {
         opacity: 1,
         y: 0,
-        transiton: {
-            duration: 0.5
+        transition: {
+            duration: 1
         }
     },
 }
@@ -29,7 +29,7 @@ const iconVariants = {
         rotateZ: 45,
         transition: {
             duration: 1,
-            delay: 1
+            delay: 0.5
         }
     }
 }
@@ -43,34 +43,36 @@ const loginVariants = {
         translateY: 0,
         transition: {
             duration: 1,
-            delay: 0.4 * i + 1
+            delay: 0.5 * i
         }
     })
 }
 
 const Login = ({ transition }) => {
     return (
-        <div className="md:grid md:grid-cols-2">
-            <motion.div initial='hidden' animate='visible' className="font-['Poppins'] bg-slate-800 text-3xl lg:text-5xl font-semibold text-gray-400 flex items-center justify-center min-h-screen">
+        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} className="py-24 sm:p-0 sm:grid sm:grid-cols-2 bg-zinc-900 sm:h-auto h-screen sm:grid-rows-1">
+            <motion.div initial='hidden' animate='visible' className="font-['Poppins'] my-10 sm:my-0 text-3xl lg:text-5xl font-semibold text-gray-400 flex items-center justify-center">
                 <motion.h1 className="relative z-1 md:text-4xl lg:text-5xl" variants={variants}>The Editor</motion.h1>
                 <motion.span variants={iconVariants} class="material-icons pl-2 lg:pl-5 md:text-4xl lg:text-8xl gray-500">description</motion.span>
             </motion.div>
-            <motion.div initial='hidden' animate='visible' className="font-['Poppins'] bg-slate-800 lg:text-5xl font-semibold flex flex-col justify-center items-center text-gray-400 min-h-screen">
+            <motion.div initial='hidden' animate='visible' className="font-['Poppins'] my-10 sm:my-0 lg:text-5xl font-semibold flex flex-col justify-center items-center text-gray-400">
                 <motion.h2 variants={loginVariants} custom={0}>Connect with:</motion.h2>
                 <MotionButton
-                    variants={loginVariants} 
-                    custom={2}
-                    color="gray"
+                    variants={loginVariants}
+                    custom={1}
+                    color='blueGray'
                     buttonType="filled"
-                    size="lg"
+                    size="md"
                     rounded={false}
                     iconOnly={false}
                     ripple="light"
+                    className='mt-3 bg-gray-700 shadow-sm'
+                    aria-label='Connect with Google'
                 >
-                    Google
+                    <GoogleSvg className="w-24 h-9" />
                 </MotionButton>
             </motion.div>
-        </div>
+        </motion.div>
     )
 }
 

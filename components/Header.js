0f3873e0b726551d530b3cf420aca4a898860f07
Anissem <dima.anissem@gmail.com>
2022-06-15 20:14:11 +0300

    Add logout dropdown

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,34 @@
-import { getSession, signOut, useSession } from 'next-auth/react'
+import { getSession, useSession } from 'next-auth/react'
 import Search from '../components/Search'
 import Logo from './Logo'
-import { motion } from 'framer-motion'
-import Tooltip from './Tooltip'
-import { useState } from 'react'
-import { auth } from '../firebase'
+
+import { useEffect, useRef, useState } from 'react'
 import { customSignOut } from './Login'
+import { motion, AnimatePresence } from 'framer-motion'
+import { Button } from '@material-tailwind/react'
 
 const Header = () => {
     const { data: session } = useSession()
-    console.log(auth.currentUser)
+    const [showAccountSettings, setShowAccountSettings] = useState(false)
+    const accountMenuRef = useRef(null)
+    const imageRef = useRef(null)
+
+    useEffect(() => {
+        const handleOuterClick = (e) => {
+            if (e.target.closest('[data-account-menu]') == accountMenuRef.current) {
+                setShowAccountSettings(true)
+            } else {
+                setShowAccountSettings(false)
+            }
+        }
+
+        document.addEventListener('click', handleOuterClick, false)
+
+        return () => {
+            document.removeEventListener('click', handleOuterClick, false)
+        }
+
+    }, [])
 
     return (
         <header className='py-3 flex items-center sticky top-0 z-50 px-5 shadow-md bg-white'>
@@ -17,14 +36,46 @@ const Header = () => {
                 <Logo />
             </div>
             <Search />
-            <div className='flex relative items-center'>
+            <div
+                ref={accountMenuRef}
+                data-account-menu
+                className='accountMenu flex relative items-center'
+            // onClick={() => { setShowAccountSettings(prev => !prev) }}
+            >
+                <AnimatePresence>
+                    {showAccountSettings && <motion.div
+                        initial={{ width: 0 }}
+                        animate={{ width: 'auto' }}
+                        exit={{ width: 0 }}
+                        className='absolute flex justify-center items-center flex-col top-[140%] right-1 bg-gray-200 shadow-2xl rounded-xl p-2 min-h-[200px] min-w-[200px]'
+                    >
+                        <motion.img
+                            width={64}
+                            height={64}
+                            className={`cursor-pointer relative rounded-full ml-2 hover:shadow-md 
+                    focus:shadow-md transition-all`}
+                            src={session.user?.image}
+                            alt="Profile image"
+                        />
+                        <h3 className='text-gray-600 my-3'>
+                            {session.user?.name}
+                        </h3>
+                        <Button
+                            color='gray'
+                            onClick={customSignOut}>
+                            Sign out
+                        </Button>
+                    </motion.div>}
+                </AnimatePresence>
                 <motion.img
-                    width={32}
-                    height={32}
-                    className='cursor-pointer rounded-full ml-2 hover:shadow-md focus:shadow-md transition-all'
+                    width={showAccountSettings ? 35 : 32}
+                    height={showAccountSettings ? 35 : 32}
+                    className={`cursor-pointer relative rounded-full ml-2 hover:shadow-md 
+                    focus:shadow-md transition-all ${showAccountSettings ? 'z-10 left-10' : ''}}`}
                     src={session.user?.image}
                     alt="Profile image"
-                    onClick={customSignOut}
+                    // ref={imageRef}
+                    // click={() => { setShowAccountSettings(prev => !prev) }}
                 />
             </div>
         </header>

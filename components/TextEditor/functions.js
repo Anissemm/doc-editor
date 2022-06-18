fb70a990cf11c61672343783a8817fd61ed9608d
Anissem <dima.anissem@gmail.com>
2022-06-18 14:30:21 +0300

    Add some optimizations

--- components/TextEditor/functions.js
+++ components/TextEditor/functions.js
@@ -1,36 +1,38 @@
 import Quill from "quill"
 import HTMLtoDOCX from 'html-to-docx-buffer'
 import FileSaver from 'file-saver'
+import debounce from 'lodash.debounce'
+import { serverTimestamp } from "firebase/firestore"
 
 const redoIcon = `<svg xmlns="http://www.w3.org/2000/svg" viewbox="0 0 48 48" ><path d="M19.6 38Q14.85 38 11.425 
                     34.8Q8 31.6 8 26.9Q8 22.2 11.425 19Q14.85 15.8 19.6 15.8H34.3L28.6 10.1L30.7 8L40 17.3L30.7 26.6L28.6 24.5L34.3 
                     18.8H19.55Q16.05 18.8 13.525 21.125Q11 23.45 11 26.9Q11 30.35 13.525 32.675Q16.05 35 19.55 35H34V38Z"/>
                     </svg>`
-                    
+
 const undoIcon = `<svg xmlns="http://www.w3.org/2000/svg" viewbox="0 0 48 48"><path d="M28.4 38H14V35H28.45Q31.95 35 34.475 
                     32.675Q37 30.35 37 26.9Q37 23.45 34.475 21.125Q31.95 18.8 28.45 18.8H13.7L19.4 24.5L17.3 26.6L8 17.3L17.3 8L19.4 
                     10.1L13.7 15.8H28.4Q33.15 15.8 36.575 19Q40 22.2 40 26.9Q40 31.6 36.575 34.8Q33.15 38 28.4 38Z"/>
                     </svg>`
 
-export const registerAttributors = ({ fontSizes, align}) => {
+export const registerAttributors = ({ fontSizes, align }) => {
     const FontSizeStyleAttributor = Quill.import('attributors/style/size')
     const AlignStyleAttributor = Quill.import('attributors/style/align')
     const BackgroundStyleAttributor = Quill.import('attributors/style/background')
     const ColorStyleAttributor = Quill.import('attributors/style/color')
     const FontStyleAttributor = Quill.import('attributors/style/font')
     const QuillIcons = Quill.import('ui/icons')
-    
+
     QuillIcons['redo'] = redoIcon
     QuillIcons['undo'] = undoIcon
     FontSizeStyleAttributor.whitelist = fontSizes
-    
+
     Quill.register(QuillIcons, true)
     Quill.register(FontSizeStyleAttributor, true)
     Quill.register(AlignStyleAttributor, true)
     Quill.register(BackgroundStyleAttributor, true)
     Quill.register(ColorStyleAttributor, true)
     Quill.register(FontStyleAttributor, true)
-    
+
     console.log(Quill.imports)
 }
 
@@ -47,3 +49,11 @@ export const setRedo = (value) => {
 export const setUndo = (value) => {
     console.log(value)
 }
+
+export const updateDocContent = debounce(async (query, content) => {
+    await query.set({
+        content: JSON.stringify(content),
+        modifiedAt: serverTimestamp()
+    }, { merge: true })
+    console.log('done')
+} , 2000) 
--- components/TextEditor/index.js
+++ components/TextEditor/index.js
@@ -1,22 +1,20 @@
 import Quill from "quill"
 import 'quill/dist/quill.snow.css'
-import { useCallback, useEffect, useState, useContext, useLayoutEffect } from "react"
+import { useCallback, useEffect, useState, useContext, memo } from "react"
 import Delta from 'quill-delta'
 import { fontSizesWithPx, toolbarOptions } from "./paramValues"
-import { registerAttributors, setRedo, setUndo } from './functions'
+import { registerAttributors, setRedo, setUndo, updateDocContent } from './functions'
 import { db } from "../../firebase"
 import { useSession } from "next-auth/react"
 import { AnimatePresence, motion } from "framer-motion"
 import { useRouter } from "next/router"
 import InfinityLoader from "../../public/svg/InfinityLoader"
-import { serverTimestamp } from "firebase/firestore"
 import { EditorContext } from "../../Providers/EditorProvider"
 import useDocumentLayoutUpdate from "../../hooks/useDocumentLayoutUpdate"
 import useQuillHistory from "../../hooks/useQuillHistory"
 
 const TextEditor = () => {
     const { data: session } = useSession()
-
     const router = useRouter()
 
     const editorCtx = useContext(EditorContext)
@@ -24,7 +22,6 @@ const TextEditor = () => {
     const [quillMounted, setQuillMounted] = editorCtx.useQuillState
 
     const [quill, setQuill] = useState(null)
-    const [content, setContent] = useState(null)
     const [overlayCompleted, setOverlayCompleted] = useState(false)
 
     const query = db.collection('userDocs').doc(session?.user?.email).collection('docs').doc(router.query.id)
@@ -34,11 +31,7 @@ const TextEditor = () => {
 
         editorSection.innerHTML = ''
 
-        const optionsToRegister = {
-            fontSizes: fontSizesWithPx
-        }
-
-        registerAttributors(optionsToRegister)
+        registerAttributors({fontSizes: fontSizesWithPx})
 
         const editorWrapper = document.createElement('div')
         editorSection.append(editorWrapper)
@@ -56,45 +49,41 @@ const TextEditor = () => {
 
         setQuill(editor)
         setQuillMounted(true)
-    }, [])
+    }, [setQuillMounted])
 
     useDocumentLayoutUpdate()
-
 
     useQuillHistory(quill)
 
     useEffect(() => {
         if (!quill) return
-        quill.on('text-change', (_delta, _oldDelta, source) => {
+        const instantTypeSaveHandler = (_delta, _oldDelta, source) => {
 
             if (source !== 'user') return
 
-            setContents(quill.getContents())
+            const content = quill.getContents()
 
-            query.set({
-                content: JSON.stringify(quill.getContents()),
-                modifiedAt: serverTimestamp()
-            }, { merge: true })
-        })
+            setContents(content)
+            updateDocContent(query, content, 1500)
+        }
+
+
+        quill.on('text-change', instantTypeSaveHandler)
+
+        return () => { quill.off('text-change', instantTypeSaveHandler) }
+
     }, [quill, query, setContents])
 
     useEffect(() => {
-        if (content && quillMounted) {
-            setContents(new Delta(content.ops))
-            quill.setContents(content)
-            quill.enable()
-        }
-    }, [content, quillMounted])
-
-    useEffect(() => {
         query.get().then(doc => {
-            const { content, layout } = doc?.data()
-            if (content) {
+            const { content } = doc?.data()
+            if (doc.exists && quillMounted && quill) {
                 const parsed = JSON.parse(content)
-                setContent(parsed)
+                setContents(new Delta(parsed.ops))
+                quill.setContents(parsed)
             }
         })
-    }, [])
+    }, [quillMounted, quill])
 
     useEffect(() => {
         if (!router.isFallback) setOverlayCompleted(true)
@@ -117,4 +106,4 @@ const TextEditor = () => {
     )
 }
 
-export default TextEditor
+export default memo(TextEditor)
